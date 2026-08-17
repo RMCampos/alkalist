247de8cf0871d1ec5fe9dbf314d0405ee7cfc593
Ricardo Campos <ricardompcampos@gmail.com>
2026-08-17 14:39:12 -0300

    fix: wrong base path for github actions

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '',
-  assetPrefix: '',
+  basePath: isProd ? '/alkalist' : '',
+  assetPrefix: isProd ? '/alkalist' : '',
   images: { unoptimized: true },
 };
 
